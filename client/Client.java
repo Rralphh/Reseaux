b5a112937325e42a11b1d4f8dcc3bb4800b67179
Ralph El Khoury <89993320+Rralphh@users.noreply.github.com>
2023-02-05 14:34:19 -0500

    Add files via upload
    Envois de commandes utilisateur/serveur avec implementation de la commande ls

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,9 @@
 package client;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 // Application client
 public class Client {
@@ -7,25 +11,52 @@ private static Socket socket;
 public static void main(String[] args) throws Exception {
 // Adresse et port du serveur
 String serverAddress = "127.0.0.1";
-int port = 5000;
+int port = 5001;
 
 // Création d'une nouvelle connexion avec le serveur
 socket = new Socket(serverAddress, port);
-
 System.out.format("Serveur lancé sur [%s:%d]", serverAddress, port);
+
 // Céation d'un canal entrant pour recevoir les messages envoyés, par le serveur
 DataInputStream in = new DataInputStream(socket.getInputStream());
 // Attente de la réception d'un message envoyé par le, server sur le canal
 String helloMessageFromServer = in.readUTF();
 System.out.println(helloMessageFromServer);
 
+while (true){
+String command;
+BufferedReader read;
 
-String waitCommand = in.readUTF();
-System.out.println(waitCommand);
+read = new BufferedReader(new InputStreamReader(System.in));
+command = read.readLine();
+
+DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+out.writeUTF(command);
+
+String responseMessage = in.readUTF();
+System.out.println(responseMessage);
+}
+
+}
+private static String askClient() throws IOException {
+	String command;
+	BufferedReader read;
 	
-
-
-// fermeture de La connexion avec le serveur
-//socket.close();
+        System.out.println("Entrer la commande");
+      
+        // envoi de commande
+        read = new BufferedReader(new InputStreamReader(System.in));
+        command = read.readLine();
+        
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF(command);
+        
+        
+        //attente de reponse
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        String waitResponse = in.readUTF();
+        System.out.println(waitResponse);
+        
+	return "commande envoyer";
 }
 }
--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -1,35 +1,95 @@
 package server;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
 private Socket socket; private int clientNumber; public ClientHandler(Socket socket, int clientNumber) {
 this.socket = socket;
 this.clientNumber = clientNumber; System.out.println("New connection with client#" + clientNumber + " at" + socket);}
 public void run() { // Création de thread qui envoi un message à un client
 try {
+InetAddress address = InetAddress.getLocalHost();
+String ipAddress = address.getHostAddress();
+int localPort = socket.getLocalPort();
+
 DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi 
-out.writeUTF("command");
 out.writeUTF("Hello from server - you re client#" + clientNumber); // envoi de message
 
+DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  //temp pour envoi console
+LocalDateTime now = LocalDateTime.now();   
+
+String commandInfo = "["+ipAddress+" : "+localPort+" - "+dtf.format(now)+"]";
+System.out.println(commandInfo+"connection etablis");
+
+while (true) {
+DataInputStream in = new DataInputStream(socket.getInputStream());
+String commandReceived = in.readUTF();
+System.out.println(commandInfo+":"+commandReceived);
+
+String[] commandSplit = commandReceived.split("\\s+");
 
 
-out.writeUTF("command");
-	//BufferedReader read;
-     //read = new BufferedReader(new InputStreamReader(System.in));
-     //String command= read.readLine();
-     /*switch (command) {
-     default:
-    	 
-    	 
-    	
-    	 
-     case "exit":
-    	 socket.close();
-    	 
-     }*/
+switch(commandSplit[0]) {
+
+case "cd":
+	out.writeUTF("commande cd recu");
+  break;
+  
+case "ls":
+	
+	
+	String directoryFile = FileSystems.getDefault().getPath(".").toString();
+	File fileObject = new File(directoryFile);
+	File[] allFiles = fileObject.listFiles();
+	String stringAllFiles="";
+	
+	for(File file : allFiles) {
+		boolean isFile=file.isFile();
+		if(isFile) {
+			
+			stringAllFiles += "File --> "+file.getName()+"\n";
+		}
+		else {
+			
+			stringAllFiles += "[Folder] --> "+ file.getName() +"\n";
+
+		}
+	}
+	
+	out.writeUTF(stringAllFiles);
+	
+  break;
+  
+case "mkdir":
+	out.writeUTF("commande mkdir recu");
+  break;
+  
+case "upload":
+	out.writeUTF("commande upload recu");
+  break;
+  
+case "download":
+	out.writeUTF("commande download recu");
+  break;
+  
+case "exit":
+	out.writeUTF("fermeture de la connection");
+	socket.close();
+	break;
+	
+default:
+	out.writeUTF("commande invalide");
+ 
+}
+}
 
 } catch (IOException e) {
 System.out.println("Error handling client# " + clientNumber + ": " + e);
@@ -38,5 +98,7 @@ try {
 socket.close();
 } catch (IOException e) {
 System.out.println("Couldn't close a socket, what's going on?");}
-System.out.println("Connection with client# " + clientNumber+ " closed");}
-}}
+System.out.println("Connection with client# " + clientNumber+ " closed");}}
+        
+}
+
