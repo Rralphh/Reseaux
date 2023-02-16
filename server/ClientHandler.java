70143b2f62f64f2e1a37dbd9da69560f74997974
Pluggedx <69322094+Pluggedx@users.noreply.github.com>
2023-02-16 10:49:16 -0500

    upload/download et gestion commandes

--- server/ClientHandler.java
+++ server/ClientHandler.java
@@ -2,8 +2,14 @@ package server;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -36,61 +42,58 @@ System.out.println(commandInfo+":"+commandReceived);
 
 String[] commandSplit = commandReceived.split("\\s+");
 
-
-switch(commandSplit[0]) {
-
-case "cd":
-	out.writeUTF("commande cd recue");
-  break;
-  
-case "ls":
-	
-	
-	String directoryFile = FileSystems.getDefault().getPath(".").toString();
-	File fileObject = new File(directoryFile);
-	File[] allFiles = fileObject.listFiles();
-	String stringAllFiles="";
-	
-	for(File file : allFiles) {
-		boolean isFile=file.isFile();
-		if(isFile) {
-			
-			stringAllFiles += "File --> "+file.getName()+"\n";
-		}
-		else {
-			
-			stringAllFiles += "[Folder] --> "+ file.getName() +"\n";
-
-		}
+String commandeClient = null;
+String demandeCommande;
+boolean actif = true;
+ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+do {
+	if (commandeClient.startsWith("cd")){
+		out.writeUTF("commande cd recue");
 	}
-	
-	out.writeUTF(stringAllFiles);
-	
-  break;
-  
-case "mkdir":
-	out.writeUTF("commande mkdir recue");
-  break;
-  
-case "upload":
-	out.writeUTF("commande upload recue");
-	
-  break;
-  
-case "download":
-	out.writeUTF("commande download recue");
+	else if (commandeClient.equals("ls")){
+		String directoryFile = FileSystems.getDefault().getPath(".").toString();
+		File fileObject = new File(directoryFile);
+		File[] allFiles = fileObject.listFiles();
+		String stringAllFiles="";
 
-  break;
-  
-case "exit":
-	out.writeUTF("fermeture de la connection");
-	socket.close();
-	break;
-	
-default:
-	out.writeUTF("commande invalide");
- 
-}
+		for(File file : allFiles) {
+			boolean isFile=file.isFile();
+			if(isFile) {
+				
+				stringAllFiles += "File --> "+file.getName()+"\n";
+			}
+			else {
+				
+				stringAllFiles += "[Folder] --> "+ file.getName() +"\n";
+
+			}
+		}
+
+		out.writeUTF(stringAllFiles);
+	}
+	else if(commandeClient.startsWith("mkdir")){
+		out.writeUTF("commande mkdir recue");
+	}
+	else if(commandeClient.startsWith("upload")){
+		File fichierUpload = demanderNomFichierUpload(reader);
+		envoyerFichierUpload(fichierUpload, objectOutputStream);
+		out.writeUTF("commande upload recue");
+	}
+	else if(commandeClient.startsWith("download")){
+		String fichierDownload = commandeClient;
+		recevoirFichierDownload(fichierDownload, objectInputStream);
+		
+		out.writeUTF("commande download recue");
+	}
+	else if(commandeClient.equals("exit")){
+		actif = false;
+		out.writeUTF("Au revoir");
+	}
+} while(true);
+
+
 }
 
 } catch (IOException e) {
@@ -101,6 +104,35 @@ socket.close();
 } catch (IOException e) {
 System.out.println("Couldn't close a socket, what's going on?");}
 System.out.println("Connection with client# " + clientNumber+ " closed");}}
-        
+
+private static File demanderNomFichierUpload(BufferedReader reader) throws IOException{
+	String nomFichier = null;
+	File fichierUpload = null;
+	nomFichier=reader.readLine();
+	fichierUpload= new File("/." + nomFichier);
+	return fichierUpload;
 }
+
+
+
+private static void envoyerFichierUpload(File fichierUpload, ObjectOutputStream objetsocketOut) throws IOException{
+	byte [] byteEnvoye = new byte [(int)fichierUpload.length()];
+
+	BufferedInputStream inputStrem = null;
+	inputStrem = new BufferedInputStream(new FileInputStream(fichierUpload));
+	inputStrem.read(byteEnvoye, 0, byteEnvoye.length);
+	inputStrem.close();
+
+}
+private static void recevoirFichierDownload(String nomFichierDownload, ObjectInputStream objetSocketIn) throws IOException
+{
+        byte [] byteRecu = (new byte [4*1024]);
+        BufferedOutputStream fichierOutputStream = null;
+        fichierOutputStream = new BufferedOutputStream( new FileOutputStream("./" + nomFichierDownload));
+        fichierOutputStream.write(byteRecu, 0, byteRecu.length);
+        fichierOutputStream.close();
+
+}
+}
+
 
